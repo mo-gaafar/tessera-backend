e3e961d37d6648b5604e2805467ecd5a85e0104f
Abdullahsaeed10 <14712019100085@stud.cu.edu.eg>
2023-03-22 00:39:11 +0200

    ready to merge

--- controller/passwordController.js
+++ controller/passwordController.js
@@ -6,7 +6,6 @@ const securePassword = require("secure-password");
 const jwt = require("jsonwebtoken");
 const { validationResult } = require("express-validator");
 const userModel = require("../models/userModel");
-// const validationResult=
 
 // this function contains configurations for sending email function
 ////////////////////////////////////////////////////////////////////////////////////////
@@ -40,87 +39,6 @@ const passwordEncryption = async (password) => {
 };
 ////////////////////////////////////////////////////////////////////////////////////////////
 
-exports.signup = async (req, res, next) => {
-  //validation
-
-  //validation
-  const { email } = req.body;
-  const { email_confirmation } = req.body;
-  // const { first_name,last_name,email,email_confirmation,password}= req.body
-
-  const userExist = await userModel.findOne({ email });
-
-  if (userExist) {
-    return res.status(400).json({
-      sucess: false,
-      message: "Email already exists",
-    });
-  }
-
-  if (email != email_confirmation) {
-    return res.status(400).json({
-      sucess: false,
-      message: "Email address does not match the above",
-    });
-  }
-
-  try {
-    const user = await userModel.create(req.body);
-    res.status(201).json({
-      success: true,
-      user,
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({
-      success: false,
-      message: error.message,
-    });
-  }
-};
-
-exports.signin = async (req, res, next) => {
-  try {
-    const { email, password } = req.body;
-    if (!email || !password) {
-      return res.status(400).json({
-        success: false,
-        message: "Email and password are required",
-      });
-    }
-
-    const user = await userModel.findOne({ email });
-    if (!user) {
-      return res.status(400).json({
-        success: false,
-        message: "Invalid Email or Password",
-      });
-    }
-
-    const isMatch = await user.comparePassword(password);
-    console.log(isMatch);
-    // use this
-    if (isMatch == securePassword.INVALID) {
-      return res.status(400).json({
-        success: false,
-        message: "Invalid Email or Password",
-      });
-    }
-    const token = await user.GenerateToken();
-
-    res.status(200).json({
-      success: true,
-      token,
-      //user,
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(400).json({
-      success: false,
-      message: "Cannot login, check your credentials",
-    });
-  }
-};
 // this function uses the userID to find the user who forgot password
 exports.resetPassword = async (req, res) => {
   try {
--- models/userModel.js
+++ models/userModel.js
@@ -6,30 +6,30 @@ const securePassword = require("secure-password");
 const jwt = require("jsonwebtoken");
 
 const UserSchema = new mongoose.Schema(
-	{
-		first_name: {
-			type: String,
-			required: true,
-			maxlength: 32,
-		},
-		last_name: {
-			type: String,
-			required: true,
-		},
-		email: {
-			type: String,
-			required: true,
-		},
-		email_confirmation: {
-			type: String,
-			required: true,
-		},
-		password: {
-			type: String,
-			required: true,
-		},
-	},
-	{ timestamps: true }
+  {
+    first_name: {
+      type: String,
+      required: true,
+      maxlength: 32,
+    },
+    last_name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    email_confirmation: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
 );
 
 // const User= mongoose.model('User',UserSchema);
@@ -37,29 +37,29 @@ const UserSchema = new mongoose.Schema(
 // module.exports= User;
 // module.exports=router;
 UserSchema.pre("save", async function (next) {
-	if (!this.isModified("password")) {
-		next();
-	}
+  if (!this.isModified("password")) {
+    next();
+  }
 
-	//this.password = await bcrypt.hash(this.password, 10);
-	this.password = passwordEncryption(this.password);
+  //this.password = await bcrypt.hash(this.password, 10);
+  this.password = passwordEncryption(this.password); // import here
 });
 
 //verify passsword
 
 UserSchema.methods.comparePassword = async function (yourPassword) {
-	//return await bcrypt.compare(yourPassword, this.password);
+  //return await bcrypt.compare(yourPassword, this.password);
 
-	const pwd = securePassword();
-	const orginalPassword = Buffer.from(yourPassword);
-	//var Hashbuf = Buffer.from(this.password);
-	const hash = await pwd.hash(orginalPassword);
+  const pwd = securePassword();
+  const orginalPassword = Buffer.from(yourPassword);
+  //var Hashbuf = Buffer.from(this.password);
+  const hash = await pwd.hash(orginalPassword);
 
-	return await pwd.verify(orginalPassword, hash);
+  return await pwd.verify(orginalPassword, hash);
 };
 
 UserSchema.methods.GenerateToken = function () {
-	return jwt.sign({ id: this.id }, process.env.SECRETJWT, { expiresIn: "3h" });
+  return jwt.sign({ id: this.id }, process.env.SECRETJWT, { expiresIn: "3h" });
 };
 
 module.exports = mongoose.model("userModel", UserSchema);
