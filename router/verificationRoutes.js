6af0ed7d29205d4dc5f74437facb0f31ee7dc74c
Abdullahsaeed10 <14712019100085@stud.cu.edu.eg>
2023-03-25 21:01:33 +0200

    json docs

--- router/verificationRoutes.js
+++ router/verificationRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 // post request
 router.post("/auth/verify/", verificationController.resendEmailVerification);
 
+// get request
 router.get("/auth/isverify/:token", verificationController.verifyEmail);
 
 module.exports = router;
