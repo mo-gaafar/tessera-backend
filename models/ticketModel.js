b8d0e3e12961d35c92507f8bf69ada9a362af65a
Abdullahsaeed10 <14712019100085@stud.cu.edu.eg>
2023-04-10 01:44:44 +0200

    prettier added

--- models/ticketModel.js
+++ models/ticketModel.js
@@ -1,47 +1,39 @@
 const mongoose = require("mongoose");
-const object=mongoose.Schema.Types.ObjectId
+const object = mongoose.Schema.Types.ObjectId;
 // creating ticket schema
-const ticketSchema = new mongoose.Schema( 
-
-{     
-     // reference to the objectID of the event, to link ticket to a certain event
-     eventID:{
-
-     type:object,
-     ref:'eventModel',
-     required:true,
-
-      },
-
-      // reference to the objectID of the user, to link ticket to user
-      userID:{
-
-      type:object,
-      ref:'userModel',
-      required:true,
-   
-      },
-
-      purchaseDate: {
-        type: String,
-        required:true,
-      },
-      purchasePrice: {
-        type: Number,
-        required:true,
-      },
-      type: {
-        type: String,
-        required:true,
-      },
-
-      // creator:req.userID
-    
-   
+const ticketSchema = new mongoose.Schema(
+  {
+    // reference to the objectID of the event, to link ticket to a certain event
+    eventID: {
+      type: object,
+      ref: "eventModel",
+      required: true,
     },
 
-   { timestamps: true },
+    // reference to the objectID of the user, to link ticket to user
+    userID: {
+      type: object,
+      ref: "userModel",
+      required: true,
+    },
 
+    purchaseDate: {
+      type: String,
+      required: true,
+    },
+    purchasePrice: {
+      type: Number,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+
+    // creator:req.userID
+  },
+
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("ticketModel", ticketSchema);
