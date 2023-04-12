8eddb1edefe679e25a7599b73ac0a71f3c793820
Abdullahsaeed10 <14712019100085@stud.cu.edu.eg>
2023-04-12 11:48:33 +0200

    temp push evenModel

--- models/eventModel.js
+++ models/eventModel.js
@@ -1,154 +1,154 @@
 const mongoose = require("mongoose");
 const { passwordEncryption } = require("../utils/passwords");
 const eventSchema = new mongoose.Schema(
-	{
-		basicInfo: {
-			eventName: {
-				type: String,
-				required: true,
-			},
-			description: String,
-			startDateTime: {
-				// timezone: String,
-				utc: Date,
-			},
-			endDateTime: {
-				timezone: String,
-				utc: Date,
-			},
-			categories: {
-				type: String,
-				enum: [
-					"Boat & Air",
-					"Business & Profession",
-					"Charity & Causes",
-					"Community & Culture",
-					"Family & Education",
-					"Fashion & Beauty",
-					"Film , Media & Entertainment",
-					"Food & Drink",
-					"Government & Politics",
-					"Health & Wellness",
-					"Hobbies & Special Interest",
-					"Home & Lifestyle",
-					"Music",
-					"Other",
-					"Performing & Visual Arts",
-					"Religion & Spirtuality",
-					"School Activities",
-					"Science & Technology",
-					"Seasonal Holiday",
-					"Sports & Fitness",
-					"Travel & Outdoor",
-				],
-				default: "Other",
-			},
-			location: {
-				longitude: {
-					type: Number,
-					required: true,
-				},
-				latitude: {
-					type: Number,
-					required: true,
-				},
-				placeId: {
-					type: Number,
-					required: true,
-				},
-				venueName: {
-					type: String,
-					required: true,
-				},
-				streetNumber: {
-					type: Number,
-					required: true,
-				},
-				route: {
-					type: String,
-					required: true,
-				},
-				administrativeAreaLevel1: {
-					type: String,
-					required: true,
-				},
-				country: {
-					type: String,
-					required: true,
-				},
-			},
-		},
+  {
+    basicInfo: {
+      eventName: {
+        type: String,
+        required: true,
+      },
+      description: String,
+      startDateTime: {
+        // timezone: String,
+        utc: Date,
+      },
+      endDateTime: {
+        timezone: String,
+        utc: Date,
+      },
+      categories: {
+        type: String,
+        enum: [
+          "Boat & Air",
+          "Business & Profession",
+          "Charity & Causes",
+          "Community & Culture",
+          "Family & Education",
+          "Fashion & Beauty",
+          "Film , Media & Entertainment",
+          "Food & Drink",
+          "Government & Politics",
+          "Health & Wellness",
+          "Hobbies & Special Interest",
+          "Home & Lifestyle",
+          "Music",
+          "Other",
+          "Performing & Visual Arts",
+          "Religion & Spirtuality",
+          "School Activities",
+          "Science & Technology",
+          "Seasonal Holiday",
+          "Sports & Fitness",
+          "Travel & Outdoor",
+        ],
+        default: "Other",
+      },
+      location: {
+        longitude: {
+          type: Number,
+        },
+        latitude: {
+          type: Number,
+        },
+        placeId: {
+          type: Number,
+        },
+        venueName: {
+          type: String,
+        },
+        streetNumber: {
+          type: Number,
+        },
+        route: {
+          type: String,
+        },
+        administrativeAreaLevel1: {
+          type: String,
+        },
+        country: {
+          type: String,
+        },
+        city: {
+          type: String,
+        },
+      },
+    },
 
-		privatePassword: String,
+    privatePassword: String,
 
-		ticketTiers: [
-			{
-				quantitySold: String,
+    ticketTiers: [
+      {
+        quantitySold: String,
 
-				capacity: Number,
+        capacity: Number,
 
-				tier: {
-					type: String,
-				},
-			},
-		],
+        tier: {
+          type: String,
+        },
+      },
+    ],
 
-		eventStatus: {
-			type: String,
-			enum: ["started", "ended", "completed", "cancelled", "live"],
-		},
-		startSelling: {
-			timezone: String,
-			utc: Date,
-		},
-		endSelling: {
-			timezone: String,
-			utc: Date,
-		},
-		publicDate: {
-			timezone: String,
-			utc: Date,
-		},
-		emailMessage: String,
+    eventStatus: {
+      type: String,
+      enum: ["started", "ended", "completed", "cancelled", "live"],
+    },
+    startSelling: {
+      timezone: String,
+      utc: Date,
+    },
+    endSelling: {
+      timezone: String,
+      utc: Date,
+    },
+    publicDate: {
+      timezone: String,
+      utc: Date,
+    },
+    emailMessage: String,
 
-		isVerified: Boolean,
+    isVerified: Boolean,
 
-		isPublic: Boolean,
+    isPublic: Boolean, // if true so the event will be public - if false the event will be private
 
-		eventQRimage: String,
+    published: Boolean, // if the creator have published the event ( it will be availble for the users to book tickets)
 
-		promoCodes: [{ code: String, percentage: Number, remainingUses: Number }],
+    eventQRimage: String,
 
-		isVerified: Boolean,
+    eventImage: String,
 
-		isPublic: Boolean,
+    creatorId: {
+      type: String,
+    },
 
-		creatorId: {
-			type: String,
-		},
+    isOnline: Boolean, // to be discussed
 
-		isOnline: Boolean, // to be discussed
-	},
+    promocode: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "promocodeModel",
+      },
+    ],
+  },
 
-	{
-		timestamps: true,
-	}
+  {
+    timestamps: true,
+  }
 );
 
 eventSchema.pre("save", async function (next) {
-	if (!this.isModified("privatePassword")) {
-		next();
-	}
-	this.privatePassword = await passwordEncryption(this.privatePassword);
+  if (!this.isModified("privatePassword")) {
+    next();
+  }
+  this.privatePassword = await passwordEncryption(this.privatePassword);
 });
 
 eventSchema.pre("findOneAndUpdate", function (next) {
-	const update = this.getUpdate();
-	if (update.basicInfo) {
-		const err = new Error("Cannot update basicInfo");
-		err.status = 403;
-		return next(err);
-	}
-	next();
+  const update = this.getUpdate();
+  if (update.basicInfo) {
+    const err = new Error("Cannot update basicInfo");
+    err.status = 403;
+    return next(err);
+  }
+  next();
 });
 module.exports = mongoose.model("eventModel", eventSchema);
