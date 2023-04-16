855995e53a7d300b80ae8a6069bce75e4ee2a627
PowerOfMagic <111122263+PowerOfMagic@users.noreply.github.com>
2023-04-17 01:53:04 +0200

    Route old 2

--- router/eventRouter.js
+++ router/eventRouter.js
@@ -2,10 +2,7 @@ const express = require("express");
 const router = express.Router();
 const eventController = require("../controller/Events/eventController");
 
-router.post(
-	"/event-management/creator/:creator-id",
-	eventController.createEvent
-);
+router.post("/event-management/creator", eventController.createEvent);
 
 router.get("/event-management/retrieve/:eventID", eventController.getEventById);
 router.delete("/event-management/delete/:eventID", eventController.deleteEvent);
