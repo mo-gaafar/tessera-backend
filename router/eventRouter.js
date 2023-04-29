a3b0ea3eca789396c794112fc2d8bed82581ca5a
AhmedOs99 <ahmed2013.ao48@gmail.com>
2023-04-29 05:25:29 +0300

    publishevent

--- router/eventRouter.js
+++ router/eventRouter.js
@@ -7,5 +7,6 @@ router.post("/event-management/creator", eventController.createEvent);
 router.get("/event-management/retrieve/:eventID", eventController.getEventById);
 router.delete("/event-management/delete/:eventID", eventController.deleteEvent);
 router.put("/event-management/update/:eventID", eventController.updateEvent);
+router.put("/event-management/publish/:eventID", eventController.publishEvent);
 
 module.exports = router; //exporting the module in order to use it in other files
