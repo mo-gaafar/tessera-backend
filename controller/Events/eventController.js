6f429a9ff2505b331791863fd0f9a61755b6fc13
AhmedOs99 <ahmed2013.ao48@gmail.com>
2023-05-02 07:22:01 +0300

    dashboard_first

--- controller/Events/eventController.js
+++ controller/Events/eventController.js
@@ -399,21 +399,13 @@ async function eventSalesByTicketType(req, res) {
     eventSales=0
     
 	if (desiredTierName=="Free"){
-      
-
 		res.status(404).json({
 			success: false,
-			message: "There are no event sales for free ticket tiers ",
-			
+			message: "There are no event sales for free ticket tiers ",		
 		   });	
-	  
-
 	}
 
-
 	else{
-
-
 
 	for (let i = 0; i < event.ticketTiers.length; i++) {
 		const tierObject = event.ticketTiers[i];
@@ -428,8 +420,6 @@ async function eventSalesByTicketType(req, res) {
 			console.log(" event quantity sold:",eventSales)
 
 		}
-
-
 
 	}
 	eventSales=Math.round(eventSales)
@@ -446,12 +436,65 @@ async function eventSalesByTicketType(req, res) {
 
 }
 
+// to be modified
+
+async function eventSoldTickets(req, res) {
+
+	const event=await eventModel.findById(req.params.eventID);
+	console.log("Event to be used in event sold tickets:",event)
+	
+
+	eventSales=0
+	totalMaxCapacity=0
+	soldTickets=0
 
 
+ 	for (let i = 0; i < event.ticketTiers.length; i++) {
+        const tierObject = event.ticketTiers[i];
+ 		const tierName=tierObject.tierName
+		console.log(`Tier ${i + 1}: ${tierName}`);
+		const maxCapacity=tierObject.maxCapacity
+		console.log(" max Capacity:",maxCapacity)
+		if (tierName=="Free"){
+			eventSales=eventSales + 0
+			totalMaxCapacity=totalMaxCapacity + maxCapacity
+			const tierQuantitySold=tierObject.quantitySold
+			console.log(" tier qs:",tierQuantitySold)
+			soldTickets=soldTickets + tierQuantitySold
+		}
+		else{
+		const tierQuantitySold=tierObject.quantitySold
+		soldTickets=soldTickets + tierQuantitySold
+		const tierPrice=tierObject.price
+		totalMaxCapacity=totalMaxCapacity + maxCapacity
+		console.log(" tier qs:",tierQuantitySold)
+		console.log(" tier price:",tierPrice)
+        eventSales=eventSales + tierQuantitySold*tierPrice
+		console.log(" event quantity sold:",eventSales)
+		}
+	}
+
+// 	}
+    eventSales=Math.round(eventSales)
+	totalMaxCapacity=Math.round(totalMaxCapacity)
+	soldTickets=Math.round(soldTickets)
+	console.log(" event quantity sold:",eventSales)
+	console.log(" total max capacity",totalMaxCapacity)
+	console.log(" sold Tickets:",soldTickets)
+	soldTicketsFromCapacity=(soldTickets/totalMaxCapacity)*100
+	soldTicketsFromCapacity=Math.round(soldTicketsFromCapacity)
+	console.log("per of sold tickets:",soldTicketsFromCapacity)
+
+	
+	res.status(200).json({
+		success: true,
+		message: "Event sold tickets as a percentage of the capacity ",
+		soldTicketsFromCapacity
+	   });	
+  
 
 
-
-
+}
 
 
 
@@ -464,5 +507,6 @@ module.exports = {
 	deleteEvent,
 	updateEvent,
 	publishEvent,
-	eventSalesByTicketType
+	eventSalesByTicketType,
+	eventSoldTickets
 };
--- router/eventRouter.js
+++ router/eventRouter.js
@@ -8,7 +8,11 @@ router.get("/event-management/retrieve/:eventID", eventController.getEventById);
 router.delete("/event-management/delete/:eventID", eventController.deleteEvent);
 router.put("/event-management/update/:eventID", eventController.updateEvent);
 router.put("/event-management/publish/:eventID", eventController.publishEvent);
-router.get("/event-management/eventsales/events/:eventID", eventController.eventSalesByTicketType);
 
+// to be modified
+router.get("/event-management/eventsales/events/:eventID", eventController.eventSalesByTicketType);
+router.get("/event-management/eventsoldtickets/events/:eventID", eventController.eventSoldTickets);
+
+//
 
 module.exports = router; //exporting the module in order to use it in other files
